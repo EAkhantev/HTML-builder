ccdc1320f8ba508e7a13dbc64ee20f8933ab91a1
e.akhantev <e.akhantev@s7.ru>
2024-01-24 22:08:50 +0400

    fix: fix a bug with updating the contents in destination folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fsp = require('fs/promises')
 const path = require('path');
 
 const folderSourceName = 'files';
@@ -7,17 +8,39 @@ const pathBase = __dirname;
 const pathSource = path.join(pathBase, folderSourceName);
 const pathToCopy = path.join(pathBase, folderToCopyName);
 
-fs.mkdir(pathToCopy, { recursive: true }, (err) =>{
-  if (err) throw err;
-});
+async function removeDir(directory) {
+  try {
+    await fsp.rmdir(directory, { recursive: true });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
 
-fs.readdir(pathSource, function (err, files) {
-  if(err) throw err;
-  files.forEach((elm) => {
-    const sourceItemPath = path.join(pathSource, elm);
-    const destinationItemPath = path.join(pathToCopy, elm);
-    fs.copyFile(sourceItemPath, destinationItemPath, err => {
-      if(err) throw err;
+async function createDir(destination) {
+  try {
+    await fsp.mkdir(destination, { recursive: true });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+function copyDir(source, destination) {
+  fs.readdir(source, function (err, files) {
+    if(err) throw err;
+    files.forEach((elm) => {
+      const sourceItemPath = path.join(source, elm);
+      const destinationItemPath = path.join(destination, elm);
+      fs.copyFile(sourceItemPath, destinationItemPath, (err) => {
+        if(err) throw err;
+      });
     });
   });
-});
+}
+
+(
+  async () => {
+    await removeDir(pathToCopy);
+    await createDir(pathToCopy);
+    copyDir(pathSource, pathToCopy);
+  }
+)();
